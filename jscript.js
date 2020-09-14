c9df10f4cc4da65bb2ff7368104725445d41c6c4
Sawon Bhattacharya <35263795+SawonBhattacharya@users.noreply.github.com>
2020-09-14 21:46:25 +0530

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -2,8 +2,8 @@
 
 const weatherApi={
 	key:"72bf0b477b905503727d7113d54031c9",
-	baseurl:"http://api.openweathermap.org/data/2.5/weather",
-	imageurl:"http://openweathermap.org/img/wn/"
+	baseurl:"https://api.openweathermap.org/data/2.5/weather",
+	imageurl:"https://openweathermap.org/img/wn/"
 }
 
 const searchInputBox=document.getElementById('input-box');
